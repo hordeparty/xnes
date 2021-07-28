41cd19cc8cdc97ccf33372104e4d6ed0cb5a39a1
Nilzao <288571+nilzao@users.noreply.github.com>
2021-07-28 11:51:40 -0400

    add video tag, stream video tag with audio, javascript keypress events

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -it --rm -v "$PWD":/src emscripten/emsdk:2.0.25 bash buildall
+docker run -it --rm -v "$PWD":/src emscripten/emsdk:2.0.25 bash -c "bash ./buildall && bash ./js_substitutions.sh"
 docker run --rm -v "$PWD"/output:/usr/share/nginx/html -p 8888:80 nginx:1.20.1
